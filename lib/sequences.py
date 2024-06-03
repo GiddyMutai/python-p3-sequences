832b8f029465510bcf045d09785fd9b10f9eb769
Gideon Mutai <giddymutai13@gmail.com>
2024-06-03 21:30:30 +0300

    Finished task

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        result = []
+    elif length == 1:
+        result = [0]
+    elif length >= 2:
+        result = [0, 1]
+        while len(result) < length:
+            sum = result[-1] + result[-2]
+            result.append(sum)
+    print(result)
+
+print_fibonacci(9)
+
+
+    
